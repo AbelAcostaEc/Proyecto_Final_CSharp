54f472669f91d6deb1a2d84169a1f463e41792f1
ASAA19971a <abel.2833@hotmail.com>
2022-01-27 23:13:31 -0500

    Crud Biometrico

--- WebApp/Controllers/BiometricosController.cs
+++ WebApp/Controllers/BiometricosController.cs
@@ -21,10 +21,60 @@ namespace WebApp.Controllers
             IEnumerable<Biometrico> listaBiometricos = db.Biometricos;
             return View(listaBiometricos);
         }
-      
+
+        //creación Biometrico
+        [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Biometrico biometrico)
+        {
+            //Grabar Biometrico
+            db.Biometricos.Add(biometrico);
+            db.SaveChanges();
+
+            TempData["mensaje"] = $"Biometrico de fecha {biometrico.Fecha} creada exitosamente";
+            return RedirectToAction("Index");
+        }
+
+        //edicion Biometrico
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Biometrico biometrico = db.Biometricos.Find(id);
+            return View(biometrico);
+        }
+        [HttpPost]
+        public IActionResult Edit(Biometrico biometrico)
+        {
+            //Grabar Biometrico
+            db.Biometricos.Update(biometrico);
+            db.SaveChanges();
+
+            TempData["mensaje"] = $"Biometrico de fecha {biometrico.Fecha} ha sido actualizada exitosamente";
+            return RedirectToAction("Index");
+        }
+
+        //borrar Biometrico
+        // presenta formulario datos Biometrico
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            Biometrico biometrico = db.Biometricos.Find(id);
+            return View(biometrico);
+        }
+        //borra Biometrico
+        [HttpPost]
+        public IActionResult Delete(Biometrico biometrico)
+        {
+            //Grabar Biometrico
+            db.Biometricos.Remove(biometrico);
+            db.SaveChanges();
+
+            TempData["mensaje"] = $"Biometrico de fecha {biometrico.Fecha} ha sido eliminada exitosamente";
+            return RedirectToAction("Index");
         }
 
     }
